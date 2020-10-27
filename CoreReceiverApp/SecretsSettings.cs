7fcc2e770e1dee1a85a83a3521a9340c1c52c855
Philip Murray <PhilipMurray99@gmail.com>
2020-10-27 14:09:49 +0000

    Add project files.

---
+++ CoreReceiverApp/SecretsSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreReceiverApp
+{
+    class SecretsSettings
+    {
+        public string ConnectionString { get; set; }
+
+        public string QueueName { get; set; }
+
+    }
+}
---
+++ CoreSenderApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreSenderApp
+{
+    class Program
+    {
+        private static string _serviceBusConnectionString = string.Empty;
+        private static string _queueName = string.Empty;
+        static IQueueClient queueClient;
+
+        public static async Task Main(string[] args)
+        {
+            var devEnvironmentVariable = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+            var isDevelopment = string.IsNullOrEmpty(devEnvironmentVariable) || devEnvironmentVariable.ToLower() == "development";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory());
+
+            if (isDevelopment)
+            {
+                builder.AddUserSecrets<Program>();
+            }
+
+            var configuration = builder.Build();
+
+            _serviceBusConnectionString = configuration["ServiceBus:ConnectionString"];
+            _queueName = configuration["ServiceBus:QueueName"];
+
+
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(_serviceBusConnectionString, _queueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send messages.
+            await SendMessagesAsync(numberOfMessages);
+
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
